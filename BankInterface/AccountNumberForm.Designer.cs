9ffeddac9a69faa00f3c22acd8d53a577007ce1e
Matt F <matthewf662@gmail.com>
2014-11-13 12:27:03 -0500

    initial file commit

---
+++ BankInterface/AccountNumberForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace BankInterface
+{
+    partial class AccountNumberForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.accountPad1 = new System.Windows.Forms.Button();
+            this.accountPad2 = new System.Windows.Forms.Button();
+            this.accountPad3 = new System.Windows.Forms.Button();
+            this.accountPad4 = new System.Windows.Forms.Button();
+            this.accountPad5 = new System.Windows.Forms.Button();
+            this.accountPad6 = new System.Windows.Forms.Button();
+            this.accountPad7 = new System.Windows.Forms.Button();
+            this.accountPad8 = new System.Windows.Forms.Button();
+            this.accountPad9 = new System.Windows.Forms.Button();
+            this.accountPad0 = new System.Windows.Forms.Button();
+            this.accountNumberBox = new System.Windows.Forms.TextBox();
+            this.accountNumberLabel = new System.Windows.Forms.Label();
+            this.accountPadOK = new System.Windows.Forms.Button();
+            this.accountPadClear = new System.Windows.Forms.Button();
+            this.accountBackspace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // accountPad1
+            // 
+            this.accountPad1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountPad1.Location = new System.Drawing.Point(20, 97);
+            this.accountPad1.Name = "accountPad1";
+            this.accountPad1.Size = new System.Drawing.Size(79, 74);
+            this.accountPad1.TabIndex = 0;
+            this.accountPad1.Text = "1";
+            this.accountPad1.UseVisualStyleBackColor = true;
+            this.accountPad1.Click += new System.EventHandler(this.accountPad1_Click);
+            // 
+            // accountPad2
+            // 
+            this.accountPad2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountPad2.Location = new System.Drawing.Point(105, 97);
+            this.accountPad2.Name = "accountPad2";
+            this.accountPad2.Size = new System.Drawing.Size(79, 74);
+            this.accountPad2.TabIndex = 1;
+            this.accountPad2.Text = "2";
+            this.accountPad2.UseVisualStyleBackColor = true;
+            this.accountPad2.Click += new System.EventHandler(this.accountPad2_Click);
+            // 
+            // accountPad3
+            // 
+            this.accountPad3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountPad3.Location = new System.Drawing.Point(190, 97);
+            this.accountPad3.Name = "accountPad3";
+            this.accountPad3.Size = new System.Drawing.Size(79, 74);
+            this.accountPad3.TabIndex = 2;
+            this.accountPad3.Text = "3";
+            this.accountPad3.UseVisualStyleBackColor = true;
+            this.accountPad3.Click += new System.EventHandler(this.accountPad3_Click);
+            // 
+            // accountPad4
+            // 
+            this.accountPad4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountPad4.Location = new System.Drawing.Point(20, 177);
+            this.accountPad4.Name = "accountPad4";
+            this.accountPad4.Size = new System.Drawing.Size(79, 74);
+            this.accountPad4.TabIndex = 3;
+            this.accountPad4.Text = "4";
+            this.accountPad4.UseVisualStyleBackColor = true;
+            this.accountPad4.Click += new System.EventHandler(this.accountPad4_Click);
+            // 
+            // accountPad5
+            // 
+            this.accountPad5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountPad5.Location = new System.Drawing.Point(105, 177);
+            this.accountPad5.Name = "accountPad5";
+            this.accountPad5.Size = new System.Drawing.Size(79, 74);
+            this.accountPad5.TabIndex = 4;
+            this.accountPad5.Text = "5";
+            this.accountPad5.UseVisualStyleBackColor = true;
+            this.accountPad5.Click += new System.EventHandler(this.accountPad5_Click);
+            // 
+            // accountPad6
+            // 
+            this.accountPad6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountPad6.Location = new System.Drawing.Point(190, 177);
+            this.accountPad6.Name = "accountPad6";
+            this.accountPad6.Size = new System.Drawing.Size(79, 74);
+            this.accountPad6.TabIndex = 5;
+            this.accountPad6.Text = "6";
+            this.accountPad6.UseVisualStyleBackColor = true;
+            this.accountPad6.Click += new System.EventHandler(this.accountPad6_Click);
+            // 
+            // accountPad7
+            // 
+            this.accountPad7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountPad7.Location = new System.Drawing.Point(20, 257);
+            this.accountPad7.Name = "accountPad7";
+            this.accountPad7.Size = new System.Drawing.Size(79, 74);
+            this.accountPad7.TabIndex = 6;
+            this.accountPad7.Text = "7";
+            this.accountPad7.UseVisualStyleBackColor = true;
+            this.accountPad7.Click += new System.EventHandler(this.accountPad7_Click);
+            // 
+            // accountPad8
+            // 
+            this.accountPad8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountPad8.Location = new System.Drawing.Point(105, 257);
+            this.accountPad8.Name = "accountPad8";
+            this.accountPad8.Size = new System.Drawing.Size(79, 74);
+            this.accountPad8.TabIndex = 7;
+            this.accountPad8.Text = "8";
+            this.accountPad8.UseVisualStyleBackColor = true;
+            this.accountPad8.Click += new System.EventHandler(this.accountPad8_Click);
+            // 
+            // accountPad9
+            // 
+            this.accountPad9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountPad9.Location = new System.Drawing.Point(190, 257);
+            this.accountPad9.Name = "accountPad9";
+            this.accountPad9.Size = new System.Drawing.Size(79, 74);
+            this.accountPad9.TabIndex = 8;
+            this.accountPad9.Text = "9";
+            this.accountPad9.UseVisualStyleBackColor = true;
+            this.accountPad9.Click += new System.EventHandler(this.accountPad9_Click);
+            // 
+            // accountPad0
+            // 
+            this.accountPad0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountPad0.Location = new System.Drawing.Point(20, 341);
+            this.accountPad0.Name = "accountPad0";
+            this.accountPad0.Size = new System.Drawing.Size(79, 74);
+            this.accountPad0.TabIndex = 9;
+            this.accountPad0.Text = "0";
+            this.accountPad0.UseVisualStyleBackColor = true;
+            this.accountPad0.Click += new System.EventHandler(this.accountPad0_Click);
+            // 
+            // accountNumberBox
+            // 
+            this.accountNumberBox.Enabled = false;
+            this.accountNumberBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountNumberBox.Location = new System.Drawing.Point(20, 47);
+            this.accountNumberBox.Name = "accountNumberBox";
+            this.accountNumberBox.Size = new System.Drawing.Size(249, 29);
+            this.accountNumberBox.TabIndex = 10;
+            // 
+            // accountNumberLabel
+            // 
+            this.accountNumberLabel.AutoSize = true;
+            this.accountNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.accountNumberLabel.Location = new System.Drawing.Point(16, 20);
+            this.accountNumberLabel.Name = "accountNumberLabel";
+            this.accountNumberLabel.Size = new System.Drawing.Size(209, 24);
+            this.accountNumberLabel.TabIndex = 11;
+            this.accountNumberLabel.Text = "Enter Account Number:";
+            // 
+            // accountPadOK
+            // 
+            this.accountPadOK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.accountPadOK.Location = new System.Drawing.Point(275, 97);
+            this.accountPadOK.Name = "accountPadOK";
+            this.accountPadOK.Size = new System.Drawing.Size(120, 41);
+            this.accountPadOK.TabIndex = 13;
+            this.accountPadOK.Text = "OK";
+            this.accountPadOK.UseVisualStyleBackColor = false;
+            this.accountPadOK.Click += new System.EventHandler(this.accountPadOK_Click);
+            // 
+            // accountPadClear
+            // 
+            this.accountPadClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.accountPadClear.Location = new System.Drawing.Point(275, 144);
+            this.accountPadClear.Name = "accountPadClear";
+            this.accountPadClear.Size = new System.Drawing.Size(120, 41);
+            this.accountPadClear.TabIndex = 14;
+            this.accountPadClear.Text = "CLEAR";
+            this.accountPadClear.UseVisualStyleBackColor = false;
+            this.accountPadClear.Click += new System.EventHandler(this.accountPadClear_Click);
+            // 
+            // accountBackspace
+            // 
+            this.accountBackspace.Location = new System.Drawing.Point(105, 386);
+            this.accountBackspace.Name = "accountBackspace";
+            this.accountBackspace.Size = new System.Drawing.Size(164, 29);
+            this.accountBackspace.TabIndex = 15;
+            this.accountBackspace.Text = "Backspace";
+            this.accountBackspace.UseVisualStyleBackColor = true;
+            this.accountBackspace.Click += new System.EventHandler(this.accountBackspace_Click);
+            // 
+            // AccountNumberForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 427);
+            this.Controls.Add(this.accountBackspace);
+            this.Controls.Add(this.accountPadClear);
+            this.Controls.Add(this.accountPadOK);
+            this.Controls.Add(this.accountNumberLabel);
+            this.Controls.Add(this.accountNumberBox);
+            this.Controls.Add(this.accountPad0);
+            this.Controls.Add(this.accountPad9);
+            this.Controls.Add(this.accountPad8);
+            this.Controls.Add(this.accountPad7);
+            this.Controls.Add(this.accountPad6);
+            this.Controls.Add(this.accountPad5);
+            this.Controls.Add(this.accountPad4);
+            this.Controls.Add(this.accountPad3);
+            this.Controls.Add(this.accountPad2);
+            this.Controls.Add(this.accountPad1);
+            this.Name = "AccountNumberForm";
+            this.Text = "Account Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button accountPad1;
+        private System.Windows.Forms.Button accountPad2;
+        private System.Windows.Forms.Button accountPad3;
+        private System.Windows.Forms.Button accountPad4;
+        private System.Windows.Forms.Button accountPad5;
+        private System.Windows.Forms.Button accountPad6;
+        private System.Windows.Forms.Button accountPad7;
+        private System.Windows.Forms.Button accountPad8;
+        private System.Windows.Forms.Button accountPad9;
+        private System.Windows.Forms.Button accountPad0;
+        private System.Windows.Forms.TextBox accountNumberBox;
+        private System.Windows.Forms.Label accountNumberLabel;
+        private System.Windows.Forms.Button accountPadOK;
+        private System.Windows.Forms.Button accountPadClear;
+        private System.Windows.Forms.Button accountBackspace;
+    }
+}
+
---
+++ BankInterface/AccountNumberForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankInterface
+{
+    public partial class AccountNumberForm : Form
+    {
+        public AccountNumberForm()
+        {
+            InitializeComponent();
+        }
+
+        private void accountPad1_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.AppendText("1");
+        }
+
+        private void accountPad2_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.AppendText("2");
+        }
+
+        private void accountPad3_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.AppendText("3");
+        }
+
+        private void accountPad4_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.AppendText("4");
+        }
+
+        private void accountPad5_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.AppendText("5");
+        }
+
+        private void accountPad6_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.AppendText("6");
+        }
+
+        private void accountPad7_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.AppendText("7");
+        }
+
+        private void accountPad8_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.AppendText("8");
+        }
+
+        private void accountPad9_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.AppendText("9");
+        }
+
+        private void accountPad0_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.AppendText("0");
+        }
+
+        private void accountPadOK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void accountPadClear_Click(object sender, EventArgs e)
+        {
+            accountNumberBox.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void accountBackspace_Click(object sender, EventArgs e)
+        {
+            if(accountNumberBox.Text.Length >0)
+            accountNumberBox.Text = accountNumberBox.Text.Remove((accountNumberBox.Text.Length - 1), 1);
+        }
+    }
+}
