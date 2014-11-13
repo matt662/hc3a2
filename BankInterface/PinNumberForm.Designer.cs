244a8f46a39644de47ab354b74eec5e1a39b6fd7
Matt F <matthewf662@gmail.com>
2014-11-13 12:50:13 -0500

    trying to add pin number form again

---
+++ BankInterface/PinNumberForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace BankInterface
+{
+    partial class PinNumberForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pinPad1 = new System.Windows.Forms.Button();
+            this.pinPad2 = new System.Windows.Forms.Button();
+            this.pinPad3 = new System.Windows.Forms.Button();
+            this.pinPad4 = new System.Windows.Forms.Button();
+            this.pinPad5 = new System.Windows.Forms.Button();
+            this.pinPad6 = new System.Windows.Forms.Button();
+            this.pinPad7 = new System.Windows.Forms.Button();
+            this.pinPad8 = new System.Windows.Forms.Button();
+            this.pinPad9 = new System.Windows.Forms.Button();
+            this.pinPad0 = new System.Windows.Forms.Button();
+            this.pinNumberBox = new System.Windows.Forms.TextBox();
+            this.pinNumberLabel = new System.Windows.Forms.Label();
+            this.pinPadOK = new System.Windows.Forms.Button();
+            this.pinPadClear = new System.Windows.Forms.Button();
+            this.pinBackspace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pinPad1
+            // 
+            this.pinPad1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinPad1.Location = new System.Drawing.Point(20, 97);
+            this.pinPad1.Name = "pinPad1";
+            this.pinPad1.Size = new System.Drawing.Size(79, 74);
+            this.pinPad1.TabIndex = 0;
+            this.pinPad1.Text = "1";
+            this.pinPad1.UseVisualStyleBackColor = true;
+            this.pinPad1.Click += new System.EventHandler(this.pinPad1_Click);
+            // 
+            // pinPad2
+            // 
+            this.pinPad2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinPad2.Location = new System.Drawing.Point(105, 97);
+            this.pinPad2.Name = "pinPad2";
+            this.pinPad2.Size = new System.Drawing.Size(79, 74);
+            this.pinPad2.TabIndex = 1;
+            this.pinPad2.Text = "2";
+            this.pinPad2.UseVisualStyleBackColor = true;
+            this.pinPad2.Click += new System.EventHandler(this.pinPad2_Click);
+            // 
+            // pinPad3
+            // 
+            this.pinPad3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinPad3.Location = new System.Drawing.Point(190, 97);
+            this.pinPad3.Name = "pinPad3";
+            this.pinPad3.Size = new System.Drawing.Size(79, 74);
+            this.pinPad3.TabIndex = 2;
+            this.pinPad3.Text = "3";
+            this.pinPad3.UseVisualStyleBackColor = true;
+            this.pinPad3.Click += new System.EventHandler(this.pinPad3_Click);
+            // 
+            // pinPad4
+            // 
+            this.pinPad4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinPad4.Location = new System.Drawing.Point(20, 177);
+            this.pinPad4.Name = "pinPad4";
+            this.pinPad4.Size = new System.Drawing.Size(79, 74);
+            this.pinPad4.TabIndex = 3;
+            this.pinPad4.Text = "4";
+            this.pinPad4.UseVisualStyleBackColor = true;
+            this.pinPad4.Click += new System.EventHandler(this.pinPad4_Click);
+            // 
+            // pinPad5
+            // 
+            this.pinPad5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinPad5.Location = new System.Drawing.Point(105, 177);
+            this.pinPad5.Name = "pinPad5";
+            this.pinPad5.Size = new System.Drawing.Size(79, 74);
+            this.pinPad5.TabIndex = 4;
+            this.pinPad5.Text = "5";
+            this.pinPad5.UseVisualStyleBackColor = true;
+            this.pinPad5.Click += new System.EventHandler(this.pinPad5_Click);
+            // 
+            // pinPad6
+            // 
+            this.pinPad6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinPad6.Location = new System.Drawing.Point(190, 177);
+            this.pinPad6.Name = "pinPad6";
+            this.pinPad6.Size = new System.Drawing.Size(79, 74);
+            this.pinPad6.TabIndex = 5;
+            this.pinPad6.Text = "6";
+            this.pinPad6.UseVisualStyleBackColor = true;
+            this.pinPad6.Click += new System.EventHandler(this.pinPad6_Click);
+            // 
+            // pinPad7
+            // 
+            this.pinPad7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinPad7.Location = new System.Drawing.Point(20, 257);
+            this.pinPad7.Name = "pinPad7";
+            this.pinPad7.Size = new System.Drawing.Size(79, 74);
+            this.pinPad7.TabIndex = 6;
+            this.pinPad7.Text = "7";
+            this.pinPad7.UseVisualStyleBackColor = true;
+            this.pinPad7.Click += new System.EventHandler(this.pinPad7_Click);
+            // 
+            // pinPad8
+            // 
+            this.pinPad8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinPad8.Location = new System.Drawing.Point(105, 257);
+            this.pinPad8.Name = "pinPad8";
+            this.pinPad8.Size = new System.Drawing.Size(79, 74);
+            this.pinPad8.TabIndex = 7;
+            this.pinPad8.Text = "8";
+            this.pinPad8.UseVisualStyleBackColor = true;
+            this.pinPad8.Click += new System.EventHandler(this.pinPad8_Click);
+            // 
+            // pinPad9
+            // 
+            this.pinPad9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinPad9.Location = new System.Drawing.Point(190, 257);
+            this.pinPad9.Name = "pinPad9";
+            this.pinPad9.Size = new System.Drawing.Size(79, 74);
+            this.pinPad9.TabIndex = 8;
+            this.pinPad9.Text = "9";
+            this.pinPad9.UseVisualStyleBackColor = true;
+            this.pinPad9.Click += new System.EventHandler(this.pinPad9_Click);
+            // 
+            // pinPad0
+            // 
+            this.pinPad0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinPad0.Location = new System.Drawing.Point(20, 341);
+            this.pinPad0.Name = "pinPad0";
+            this.pinPad0.Size = new System.Drawing.Size(79, 74);
+            this.pinPad0.TabIndex = 9;
+            this.pinPad0.Text = "0";
+            this.pinPad0.UseVisualStyleBackColor = true;
+            this.pinPad0.Click += new System.EventHandler(this.pinPad0_Click);
+            // 
+            // pinNumberBox
+            // 
+            this.pinNumberBox.Enabled = false;
+            this.pinNumberBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinNumberBox.Location = new System.Drawing.Point(20, 47);
+            this.pinNumberBox.Name = "pinNumberBox";
+            this.pinNumberBox.PasswordChar = '*';
+            this.pinNumberBox.Size = new System.Drawing.Size(249, 29);
+            this.pinNumberBox.TabIndex = 10;
+            // 
+            // pinNumberLabel
+            // 
+            this.pinNumberLabel.AutoSize = true;
+            this.pinNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.pinNumberLabel.Location = new System.Drawing.Point(16, 20);
+            this.pinNumberLabel.Name = "pinNumberLabel";
+            this.pinNumberLabel.Size = new System.Drawing.Size(166, 24);
+            this.pinNumberLabel.TabIndex = 11;
+            this.pinNumberLabel.Text = "Enter Pin Number:";
+            // 
+            // pinPadOK
+            // 
+            this.pinPadOK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pinPadOK.Location = new System.Drawing.Point(275, 97);
+            this.pinPadOK.Name = "pinPadOK";
+            this.pinPadOK.Size = new System.Drawing.Size(120, 41);
+            this.pinPadOK.TabIndex = 13;
+            this.pinPadOK.Text = "OK";
+            this.pinPadOK.UseVisualStyleBackColor = false;
+            this.pinPadOK.Click += new System.EventHandler(this.pinPadOK_Click);
+            // 
+            // pinPadClear
+            // 
+            this.pinPadClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.pinPadClear.Location = new System.Drawing.Point(275, 144);
+            this.pinPadClear.Name = "pinPadClear";
+            this.pinPadClear.Size = new System.Drawing.Size(120, 41);
+            this.pinPadClear.TabIndex = 14;
+            this.pinPadClear.Text = "CLEAR";
+            this.pinPadClear.UseVisualStyleBackColor = false;
+            this.pinPadClear.Click += new System.EventHandler(this.pinPadClear_Click);
+            // 
+            // pinBackspace
+            // 
+            this.pinBackspace.Location = new System.Drawing.Point(105, 386);
+            this.pinBackspace.Name = "pinBackspace";
+            this.pinBackspace.Size = new System.Drawing.Size(164, 29);
+            this.pinBackspace.TabIndex = 15;
+            this.pinBackspace.Text = "Backspace";
+            this.pinBackspace.UseVisualStyleBackColor = true;
+            this.pinBackspace.Click += new System.EventHandler(this.pinBackspace_Click);
+            // 
+            // PinNumberForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 427);
+            this.Controls.Add(this.pinBackspace);
+            this.Controls.Add(this.pinPadClear);
+            this.Controls.Add(this.pinPadOK);
+            this.Controls.Add(this.pinNumberLabel);
+            this.Controls.Add(this.pinNumberBox);
+            this.Controls.Add(this.pinPad0);
+            this.Controls.Add(this.pinPad9);
+            this.Controls.Add(this.pinPad8);
+            this.Controls.Add(this.pinPad7);
+            this.Controls.Add(this.pinPad6);
+            this.Controls.Add(this.pinPad5);
+            this.Controls.Add(this.pinPad4);
+            this.Controls.Add(this.pinPad3);
+            this.Controls.Add(this.pinPad2);
+            this.Controls.Add(this.pinPad1);
+            this.Name = "PinNumberForm";
+            this.Text = "Pin Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pinPad1;
+        private System.Windows.Forms.Button pinPad2;
+        private System.Windows.Forms.Button pinPad3;
+        private System.Windows.Forms.Button pinPad4;
+        private System.Windows.Forms.Button pinPad5;
+        private System.Windows.Forms.Button pinPad6;
+        private System.Windows.Forms.Button pinPad7;
+        private System.Windows.Forms.Button pinPad8;
+        private System.Windows.Forms.Button pinPad9;
+        private System.Windows.Forms.Button pinPad0;
+        private System.Windows.Forms.TextBox pinNumberBox;
+        private System.Windows.Forms.Label pinNumberLabel;
+        private System.Windows.Forms.Button pinPadOK;
+        private System.Windows.Forms.Button pinPadClear;
+        private System.Windows.Forms.Button pinBackspace;
+    }
+}
+
---
+++ BankInterface/PinNumberForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankInterface
+{
+    public partial class PinNumberForm : Form
+    {
+        public PinNumberForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pinPad1_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.AppendText("1");
+        }
+
+        private void pinPad2_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.AppendText("2");
+        }
+
+        private void pinPad3_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.AppendText("3");
+        }
+
+        private void pinPad4_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.AppendText("4");
+        }
+
+        private void pinPad5_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.AppendText("5");
+        }
+
+        private void pinPad6_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.AppendText("6");
+        }
+
+        private void pinPad7_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.AppendText("7");
+        }
+
+        private void pinPad8_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.AppendText("8");
+        }
+
+        private void pinPad9_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.AppendText("9");
+        }
+
+        private void pinPad0_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.AppendText("0");
+        }
+
+        private void pinPadOK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pinPadClear_Click(object sender, EventArgs e)
+        {
+            pinNumberBox.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pinBackspace_Click(object sender, EventArgs e)
+        {
+            if(pinNumberBox.Text.Length >0)
+            pinNumberBox.Text = pinNumberBox.Text.Remove((pinNumberBox.Text.Length - 1), 1);
+        }
+    }
+}
